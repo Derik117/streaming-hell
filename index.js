9c0589c06f0f2d5ba7e436a79a984b6d8d7610df
Derik <ideriki@gmail.com>
2019-02-19 20:37:36 +0300

    refactoring and pretty message view :)

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 require('dotenv').config();
 const Telegraf = require('telegraf');
 const getData = require('./getData');
+const getMessageText = require('./messageProcessing');
 const parseURL = require('./parseURL');
 const validator = require('validator');
-const readableNames = {
-    yandex: 'Yandex',
-    spotify: 'Spotify',
-    appleMusic: 'Apple Music',
-    youtubeMusic: 'YouTube Music',
-    youtube: 'YouTube',
-    pandora: 'Pandora',
-    google: 'Google',
-    deezer: 'Deezer',
-    tidal: 'Tidal',
-    napster: 'Napster',
-    fanburst: 'Fanburst',
-};
+
 const bot = new Telegraf(process.env.TOKEN);
 bot.start((ctx) => {
     ctx.reply('👋 Привет!\n\nПоделись со мной ссылкой на трек или альбом из любого приложения, а я в ответ пришлю ссылки, на все музыкальные сервисы где можно найти этот альбом или композицию.')
@@ -35,22 +24,8 @@ bot.on('message', async (ctx) => {
                     ctx.reply('🚬 Подождите немного, пока я ищу ссылки...');
                     const data = await getData({link: urls[0]});
                     if (data) {
-                        let links = '';
-                        data.songlink.links.listen.sort((a, b) => {
-                            const nameA = readableNames[a.name] || a.name;
-                            const nameB = readableNames[b.name] || b.name;
-                            if (nameA > nameB) {
-                                return 1;
-                            } else if (nameA < nameB) {
-                                return -1;
-                            }
-                            return 0;
-                        });
-                        data.songlink.links.listen.forEach(item => {
-                            const name = readableNames[item.name] || item.name;
-                            links = `${links}\n[${name}](${item.data.listenUrl})\n`;
-                        });
-                        ctx.reply(links, {parse_mode: 'markdown'});
+                        let messageText = getMessageText({data});
+                        ctx.reply(messageText, {parse_mode: 'markdown'});
                         ctx.reply('👋 Готово!');
                     } else {
                         ctx.reply('😣 Кажется у меня нет данных по этой ссылке. Убедись, что адрес верный.');
